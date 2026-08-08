3f53b1cd88b92eda420a64a9344a37d6ba0e9cbe
Lished <lished.pro@outlook.com>
2026-08-08 10:40:04 +0200

    Remove unused AddNode and getOrCreateUser functions from supabase.go

--- data/database/supabase/supabase.go
+++ data/database/supabase/supabase.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"net"
 	"time"
 
 	"github.com/google/uuid"
@@ -29,40 +28,6 @@ func InitDatabase(connString string) error {
 	}
 	SupabaseDB = db
 	return nil
-}
-
-func AddNode(db *sqlx.DB, uid string, nodeId string) error {
-	_, err := getOrCreateUser(db, uid)
-	if err != nil {
-		return err
-	}
-	nodeIP, _, err := net.SplitHostPort(nodeId)
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec("INSERT INTO \"UserNodes\" (\"authUserId\", \"id\", \"createdAt\") VALUES ($1, $2, $3)", uid, nodeIP, time.Now())
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// getOrCreateUser checks if a user exists by UID, creates if not
-func getOrCreateUser(db *sqlx.DB, uid string) (*UserData, error) {
-	var user UserData
-	err := db.Get(&user, "SELECT \"authUserId\", \"createdAt\", \"updatedAt\" FROM \"UserData\" WHERE \"authUserId\"=$1", uid)
-	if err == nil {
-		return &user, nil // User exists
-	}
-	// If not found, create
-	now := time.Now()
-	_, err = db.Exec("INSERT INTO \"UserData\" (\"authUserId\", \"createdAt\", \"updatedAt\") VALUES ($1, $2, $3)", uid, now, now)
-	if err != nil {
-		return nil, err
-	}
-	user = UserData{AuthUserId: uid, CreatedAt: now, UpdatedAt: now}
-	return &user, nil
 }
 
 // CreateNodeConnectRequest inserts a pairing UUID for a node IP (TTL = 2 hours).
